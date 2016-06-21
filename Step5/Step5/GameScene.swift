3f537a15b19611fb453d66749ac1d93b39a0afa3
omura.522 <omura.522@fssoftware.co.jp>
2016-06-21 10:41:22 +0900

    Update Step5

--- Step5/Step5/GameScene.swift
+++ Step5/Step5/GameScene.swift
@@ -11,16 +11,26 @@ import GameplayKit
 
 class GameScene: SKScene {
     
-    var baseNode: SKNode!
-    
-    func setupBackground() {
-        // 背景画像を読み込む
+    func setupBackground(baseNode: SKNode) {
+        // 背景画像からテクスチャを作成
         let texture = SKTexture(imageNamed: "background")
+        // フィルタリングモード（テクスチャの本来のサイズ以外で描画される場合に使用される）
+        // .Linear クォリティーの高い描画ができるけど遅い
+        // .Nearest 粗いけど速い
         texture.filteringMode = .nearest
         
         // 必要な画像枚数を算出
-        let needHeightNumber = ceil( 2.0 + (self.frame.size.height / texture.size().height) )
-        let needWidthNumber = ceil( 2.0 + self.frame.size.width / texture.size().width )
+        let needHeightNumber = ceil(2.0 + (self.frame.size.height / texture.size().height))
+        let needWidthNumber = ceil(self.frame.size.width / texture.size().width)
+        
+        print("frame height:\(self.frame.size.height)")
+        print("frame width:\(self.frame.size.width)")
+        
+        print("texture height:\(texture.size().height)")
+        print("texture width:\(texture.size().width)")
+        
+        print("needHeightNumber:\(needHeightNumber)")
+        print("needWidthNumber:\(needWidthNumber)")
         
         // アニメーションを作成
         let moveAnim = SKAction.moveBy(x: 0.0, y: -texture.size().height, duration: TimeInterval(texture.size().height / 10.0))
@@ -28,21 +38,19 @@ class GameScene: SKScene {
         let repeatForeverAnim = SKAction.repeatForever(SKAction.sequence([moveAnim, resetAnim]))
         
         // 画像の配置とアニメーションを設定
-        for i in 0 ..< Int(needWidthNumber) {
-            for j in 0 ..< Int(needHeightNumber) {
-                let sprite = SKSpriteNode(texture: texture)
-                sprite.zPosition = -100.0
-                sprite.position = CGPoint(x: CGFloat(i)*sprite.size.width, y: CGFloat(j)*sprite.size.height)
-                sprite.run(repeatForeverAnim)
-                self.baseNode.addChild(sprite)
-            }
+        for j in 0 ..< Int(needHeightNumber) {
+            let sprite = SKSpriteNode(texture: texture)
+            sprite.zPosition = -100.0
+            sprite.position = CGPoint(x:self.frame.midX, y: CGFloat(j)*sprite.size.height)
+            sprite.run(repeatForeverAnim)
+            baseNode.addChild(sprite)
         }
     }
+    
     override func didMove(to view: SKView) {
-        baseNode = SKNode()
+        let baseNode = SKNode()
         baseNode.speed = 1.0
+        self.setupBackground(baseNode: baseNode)
         self.addChild(baseNode)
-        
-        self.setupBackground()
     }
 }
